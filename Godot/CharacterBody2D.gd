2612d912cae50adb21d18b2748dd850fde91fb02
CostnerMagill <78157985+CostnerMagill@users.noreply.github.com>
2024-02-10 18:16:31 -0500

    Claire sprite is moving around with camera tracking. Animations are still kind of messed up but directions are working.

---
+++ Godot/CharacterBody2D.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	
+func _ready():
+	pass # Replace with function body.
+@onready var _animated_sprite = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if Input.is_action_pressed("ui_up"):
+		_animated_sprite.play("Run forward")
+	else:
+		_animated_sprite.stop()
+	
+	if Input.is_action_pressed("ui_down"):
+		_animated_sprite.play("Run backward")
+	else:
+		_animated_sprite.stop()
+		
+	if Input.is_action_pressed("ui_left"):
+		_animated_sprite.play("Run left")
+	else:
+		_animated_sprite.stop()
+	
+	if Input.is_action_pressed("ui_right"):
+		_animated_sprite.play("Run right")
+	else:
+		_animated_sprite.stop()
+
