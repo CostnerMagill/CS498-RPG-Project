dd4913100f534cfe6f5c71ba86d4885d24c31efc
CostnerMagill <78157985+CostnerMagill@users.noreply.github.com>
2024-03-22 00:02:41 -0400

    Fixed animations and movement speed. Also changed movement to WASD

--- Godot/CharacterBody2D.gd
+++ Godot/CharacterBody2D.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 75
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -17,12 +17,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_pressed("ui_up"):
-		_animated_sprite.play("RunForward")
+		_animated_sprite.play("WalkForward")
 	elif Input.is_action_pressed("ui_down"):
-		_animated_sprite.play("RunBackward")
+		_animated_sprite.play("WalkBackward")
 	elif Input.is_action_pressed("ui_left"):
-		_animated_sprite.play("RunLeft")
+		_animated_sprite.play("WalkLeft")
 	elif Input.is_action_pressed("ui_right"):
-		_animated_sprite.play("RunRight")
+		_animated_sprite.play("WalkRight")
 	else:
 		_animated_sprite.stop()
+
